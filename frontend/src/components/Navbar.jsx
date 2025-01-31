ba73ae36c77d77400b872cf22633384e8e5aa8a7
Mohit Soni <mohitsoni2006ms@gmail.com>
2025-01-31 15:11:09 +0530

    Initializing frontend folder

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavbarComponent = () => {
+  return (
+    <div className='text-3xl text-orange-500'>NavbarComponent</div>
+  )
+}
+
+export default NavbarComponent
