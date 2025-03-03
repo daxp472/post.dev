3f12880827b44ef3f7fbc729c06b57a346f48266
Mohit Soni <mohitsoni2006ms@gmail.com>
2025-03-03 10:12:17 +0530

    fixing the error

--- backend/utils/ChangePostProfileImge.js
+++ backend/utils/ChangePostProfileImge.js
@@ -1,5 +1,5 @@
-import Post from "../models/post.model";
-import User from "../models/user.model";
+import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
 
 export const updateUserImageInPosts = async (userId, newImageUrl) => {
     try {
